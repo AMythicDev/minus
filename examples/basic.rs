d0143b6a483b3dad585cafa0979d4812751269f6
Arijit Dey <arijid79@gmail.com>
2020-11-29 16:49:10 +0530

    Rewrite using crossterm

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,19 +1,19 @@
+use async_std::task::sleep;
 use futures::{executor::block_on, join};
 use minus::*;
 use std::sync::mpsc::channel;
 use std::time::Duration;
-use async_std::task::sleep;
 
 fn main() {
     let (tx, cx) = channel();
     let increment = async {
         let mut counter = 0;
-        while counter <= 5 {
-            tx.send(Signal::Data(counter));
+        while counter <= 100 {
+            let _ = tx.send(Signal::Data(counter.to_string()));
             counter += 1;
-            sleep(Duration::from_millis(1000)).await;
+            sleep(Duration::from_millis(100)).await;
         }
-        tx.send(Signal::Close);
+        let _ = tx.send(Signal::Close);
     };
     let run = async {
         join!(increment, run(cx));
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,18 @@
 /// TODO: Clean up this section
 use async_std::task::sleep;
+use crossterm::{
+    cursor::{MoveTo, MoveToColumn},
+    event::{poll, read, Event, KeyCode, KeyEvent, KeyModifiers},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+    terminal::{Clear, ClearType},
+};
+use futures::join;
+use std::cell::RefCell;
+use std::io::{prelude::*, stdout};
+use std::rc::Rc;
 use std::sync::mpsc::Receiver;
 use std::time::Duration;
-use termion::screen::AlternateScreen;
-use termion::raw::IntoRawMode;
-use std::io::{stdout, prelude::*};
-use termion::{input::TermRead, event::Key};
-use termion::async_stdin;
-use termion::cursor::Left;
-use termion::clear::All;
 
 /// Different signals that could be sent to the pager
 ///
@@ -21,45 +25,84 @@ use termion::clear::All;
 /// TODO
 pub enum Signal<T>
 where
-    T: std::fmt::Display,
+    T: std::fmt::Display + Into<String> + Clone,
 {
     Data(T),
     Close,
 }
 
-pub async fn run<T>(cx: Receiver<Signal<T>>)
+type Lines = Rc<RefCell<Vec<String>>>;
+
+async fn update_string<T>(cx: Receiver<Signal<T>>, lines: Lines)
 where
-    T: std::fmt::Display,
+    T: std::fmt::Display + Into<String> + Clone,
 {
-    let stdout = stdout().into_raw_mode().unwrap();
-    let mut screen = AlternateScreen::from(stdout);
-    let mut check = true;
-    let mut keys = async_stdin().keys();
     loop {
-        if check {
-            match cx.try_iter().peekable().peek() {
-                Some(Signal::Data(text)) => {
-                    write!(screen, "{}", text);
-                    writeln!(screen, "{}", Left(1));
-                }
-                Some(Signal::Close) => {
-                    check = false;
-                }
-                None => {
-                    sleep(Duration::from_millis(500)).await;
-                }
+        match cx.try_recv() {
+            Ok(Signal::Data(text)) => {
+                let mut lines = lines.borrow_mut();
+                let string: String = text.into();
+                let buf_line: Vec<&str> = string.split_terminator('\n').collect();
+                buf_line.iter().for_each(|item| {
+                    lines.push(item.to_string());
+                });
+                drop(lines);
+                sleep(Duration::from_millis(500)).await;
             }
-        }
-        match keys.next() {
-            Some(Ok(Key::Char('q'))) =>{
-                drop(screen);
-                std::process::exit(0);  
-            },
-            Some(Ok(_)) => {},
-            Some(Err(_)) => println!("Invalid Key"),
-            None => {
-                continue;
+            Ok(Signal::Close) => {
+                break;
             }
+            Err(_) => {}
         }
     }
+}
+
+async fn draw(lines: Lines) {
+    let _ = execute!(stdout(), EnterAlternateScreen);
+    let _ = enable_raw_mode();
+    let mut last_printed = 0;
+
+    loop {
+        let borrow = lines.borrow();
+        if last_printed < borrow.len() {
+            print!("{}{}", Clear(ClearType::All), MoveTo(0, 0),);
+            for line in borrow.iter() {
+                println!("{}{}", line, MoveToColumn(0));
+            }
+            last_printed = borrow.len();
+        }
+        drop(borrow);
+
+        if poll(Duration::from_millis(500)).unwrap() {
+            match read().unwrap() {
+                Event::Key(KeyEvent {
+                    code: KeyCode::Char('q'),
+                    modifiers: KeyModifiers::NONE,
+                }) | Event::Key(KeyEvent {
+                    code: KeyCode::Char('c'),
+                    modifiers: KeyModifiers::CONTROL
+                }) => {
+                    let _ = execute!(stdout(), LeaveAlternateScreen);
+                    let _ = disable_raw_mode();
+                    std::process::exit(0);
+                }
+                _ => {}
+            }
+        }
+        sleep(Duration::from_millis(500)).await;
+    }
+}
+
+/*
+*/
+
+pub async fn run<T>(cx: Receiver<Signal<T>>)
+where
+    T: std::fmt::Display + Into<String> + Clone,
+{
+    let lines: Lines = Rc::new(RefCell::new(Vec::new()));
+    join!(update_string(cx, lines.clone()), draw(lines));
+
+    // let stdout = stdout().into_raw_mode().unwrap();
+    // let height = termion::terminal_size().unwrap().1;
 }
