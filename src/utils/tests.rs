23f149a4b2812cb5ae2113d33a7dd1c126ddbab7
Alexis Bourget <alexis.bourget@gmail.com>
2020-12-04 17:26:31 +0100

    Add comments and one more subtest for utils

--- src/utils/tests.rs
+++ src/utils/tests.rs
@@ -35,6 +35,7 @@ fn short_no_line_numbers() {
 fn long_no_line_numbers() {
     let lines = "A line\nAnother line\nThird line\nFourth line";
 
+    // Displaying as much of the lines as possible from the start.
     let mut out = Vec::with_capacity(lines.len());
     let mut upper_mark = 0;
     let rows = 3;
@@ -47,8 +48,23 @@ fn long_no_line_numbers() {
     );
     assert_eq!(upper_mark, 0);
 
+    // This ensures that asking for a position other than 0 works.
     let mut out = Vec::with_capacity(lines.len());
     let mut upper_mark = 1;
+    let rows = 3;
+
+    assert!(write_lines(&mut out, lines, rows, &mut upper_mark, LineNumbers::No).is_ok());
+
+    assert_eq!(
+        "Another line\nThird line\nFourth line\n",
+        String::from_utf8(out).expect("Should have written valid UTF-8")
+    );
+    assert_eq!(upper_mark, 1);
+
+    // This test ensures that as much text as possible will be displayed, even
+    // when less is asked for.
+    let mut out = Vec::with_capacity(lines.len());
+    let mut upper_mark = 2;
     let rows = 3;
 
     assert!(write_lines(&mut out, lines, rows, &mut upper_mark, LineNumbers::No).is_ok());
