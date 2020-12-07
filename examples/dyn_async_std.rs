a612eb0a57cfbdbbc29bc50506b0a8b0efc7b51f
Alexis Bourget <alexis.bourget@gmail.com>
2020-12-07 23:35:26 +0100

    Use Result in the examples to motivate proper use

--- examples/dyn_async_std.rs
+++ examples/dyn_async_std.rs
@@ -1,22 +1,28 @@
 use async_std::task::sleep;
 use futures::join;
-use minus::*;
+
 use std::fmt::Write;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
 
 #[async_std::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let output = Arc::new(Mutex::new(String::new()));
+
     let increment = async {
         let mut counter: u8 = 0;
         while counter <= 30 {
             let mut output = output.lock().unwrap();
-            let _ = writeln!(output, "{}", counter.to_string());
+            writeln!(output, "{}", counter.to_string())?;
             counter += 1;
             drop(output);
             sleep(Duration::from_millis(100)).await;
         }
+        Result::<_, std::fmt::Error>::Ok(())
     };
-    join!(async_std_updating(output.clone()), increment);
+
+    let (res1, res2) = join!(minus::async_std_updating(output.clone()), increment);
+    res1?;
+    res2?;
+    Ok(())
 }
--- examples/dyn_tokio.rs
+++ examples/dyn_tokio.rs
@@ -1,22 +1,28 @@
 use futures::join;
-use minus::*;
+use tokio::time::sleep;
+
 use std::fmt::Write;
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
-use tokio::time::sleep;
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let output = Arc::new(Mutex::new(String::new()));
+
     let increment = async {
         let mut counter: u8 = 0;
         while counter <= 30 {
             let mut output = output.lock().unwrap();
-            let _ = writeln!(output, "{}", counter.to_string());
+            writeln!(output, "{}", counter.to_string())?;
             counter += 1;
             drop(output);
             sleep(Duration::from_millis(100)).await;
         }
+        Result::<_, std::fmt::Error>::Ok(())
     };
-    join!(tokio_updating(output.clone()), increment);
+
+    let (res1, res2) = join!(minus::tokio_updating(output.clone()), increment);
+    res1?;
+    res2?;
+    Ok(())
 }
