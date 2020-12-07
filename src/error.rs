890cca67e2e271c4e038b258e57520de802569f2
Alexis Bourget <alexis.bourget@gmail.com>
2020-12-07 12:26:35 +0100

    Prepare to handle results by adding an error type

---
+++ src/error.rs
@@ -0,0 +1,80 @@
+//! See [`Error`] and [`Result`].
+use crossterm;
+
+use std::fmt;
+use std::io;
+
+/// Type alias for easier use of errors produced by [`minus`](crate).
+pub type Result<T = (), E = Error> = std::result::Result<T, E>;
+
+/// Global error type for [`minus`](crate).
+///
+/// You can get more informations about this error by calling
+/// [`source`](std::error::Error::source) on it.
+#[derive(Debug)]
+pub enum Error {
+    /// The error is an IO one, for example locking `stdout` failed.
+    IoError(io::Error),
+    /// An operation on the terminal failed, for example resizing it.
+    TermError(TermError),
+}
+
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Self::IoError(e) => Some(e),
+            Self::TermError(e) => e.source(),
+        }
+    }
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::IoError(e) => write!(fmt, "IO-error occurred: {}", e),
+            Self::TermError(e) => write!(fmt, "Operation on terminal failed: {}", e),
+        }
+    }
+}
+
+/// An operation on the terminal failed, for example resizing it.
+///
+/// You can get more informations about this error by calling
+/// [`source`](std::error::Error::source) on it.
+#[derive(Debug)]
+pub struct TermError(
+    // This member is private to avoid leaking the crossterm error type up the
+    // dependency chain.
+    crossterm::ErrorKind,
+);
+
+impl std::error::Error for TermError {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        self.0.source()
+    }
+}
+
+impl fmt::Display for TermError {
+    fn fmt(&self, fmt: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(fmt, "{}", self.0)
+    }
+}
+
+macro_rules! impl_from {
+    ($from:path, $to:expr) => {
+        impl From<$from> for crate::Error {
+            fn from(e: $from) -> Self {
+                $to(e)
+            }
+        }
+    };
+}
+
+impl_from!(io::Error, Error::IoError);
+impl_from!(TermError, Error::TermError);
+
+impl From<crossterm::ErrorKind> for crate::Error {
+    fn from(e: crossterm::ErrorKind) -> Self {
+        Self::TermError(TermError(e))
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,38 @@
 //! Minus is a library for creating paged output for other terminal based
 //! applications. It is threaded as well as asynchronous, which means your
-//! applications can give dynamic information. It is also cross-platform
-//! which means your applications are assured to be 100% compatible with all OSs
+//! applications can give dynamic information. It is also cross-platform which
+//! means your applications are assured to be 100% compatible with all OSs.
 //!
 //! ## Why use minus
-//! * Pager runs in a separate thread which is asynchronous
-//! * Works with both tokio and `async_std`, these are individual features you can
-//! enable. So you are confirmed that you don't put bloat in your software
-//! * Completely cross-platform
+//!
+//! * Pager can run in a separate thread which is asynchronous (when using
+//! either the `async_std_lib` or `tokio_lib` feature).
+//! * Works with both tokio and `async_std`, these are individual features you
+//! can enable. So you are confirmed that you don't put bloat in your software.
+//! * Completely cross-platform.
 //!
 //! ## Features
-//! * `async_std_lib`:- If your application uses async-std, enable this feature
-//! * `tokio_lib`:- If your application uses tokio, enable this feature
-//! * `static_output`: Enable this if you only want to page static data
+//!
+//! * `async_std_lib`: If your application uses async-std, enable this feature.
+//! * `tokio_lib`: If your application uses tokio, enable this feature.
+//! * `static_output`: Enable this if you only want to page static data.
 //!
 //! ## Examples
+//!
 //! See [`page_all`] for static output examples or [`async_std_updating`] and
-//! [`tokio_updating`] for examples of dynamic output generation using different runtimes
+//! [`tokio_updating`] for examples of dynamic output generation using
+//! different runtimes.
 
 #![allow(unused_imports)]
 #![allow(dead_code)]
 #![deny(clippy::all)]
 #![warn(clippy::pedantic)]
 
-mod utils;
 use std::sync::{Arc, Mutex};
+
+mod error;
+mod utils;
+
 #[cfg(any(feature = "tokio_lib", feature = "async_std_lib"))]
 mod rt_wrappers;
 #[cfg(feature = "static_output")]
@@ -32,6 +40,8 @@ mod static_pager;
 
 /// An atomically reference counted string of all output for the pager
 pub type Lines = Arc<Mutex<String>>;
+
+pub use error::{Error, Result};
 
 #[cfg(feature = "tokio_lib")]
 pub use rt_wrappers::tokio_updating;
