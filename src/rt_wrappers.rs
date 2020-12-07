899f4c027e74c68c5da58958f27c4185f2c6ca9c
Alexis Bourget <alexis.bourget@gmail.com>
2020-12-07 23:34:24 +0100

    Use minus::Result everywhere it's possible

--- src/rt_wrappers.rs
+++ src/rt_wrappers.rs
@@ -1,5 +1,8 @@
+//! Dynamic information within a pager window.
+//!
+//! See [`tokio_updating`] and [`async_std_updating`] for more information.
 use crate::utils::draw;
-use crate::Lines;
+use crate::{Lines, Result};
 
 use crossterm::{
     cursor::{Hide, Show},
@@ -11,12 +14,11 @@ use crossterm::{
 use std::io::{prelude::*, stdout};
 use std::time::Duration;
 
-fn init(mutex: &Lines) {
+fn init(mutex: &Lines) -> Result {
     // Initialize the terminal
-    // FIXME(poliorcetics): handle unused Result.
-    let _ = execute!(stdout(), EnterAlternateScreen);
-    let _ = enable_raw_mode();
-    let _ = execute!(stdout(), Hide);
+    execute!(stdout(), EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    execute!(stdout(), Hide)?;
 
     // Get terminal rows and convert it to usize
     let (_, rows) = crossterm::terminal::size().unwrap();
@@ -38,8 +40,7 @@ fn init(mutex: &Lines) {
         let string = string.unwrap();
         // Use .eq() here as == cannot compare MutexGuard with a normal string
         if !string.eq(&last_copy) {
-            // FIXME(poliorcetics): handle unused Result.
-            let _ = draw(&string, rows, &mut upper_mark);
+            draw(&string, rows, &mut upper_mark)?;
             // Update the last copy, cloning here becaue string is inside MutexGuard
             last_copy = string.to_string();
         }
@@ -58,9 +59,9 @@ fn init(mutex: &Lines) {
                     code: KeyCode::Char('c'),
                     modifiers: KeyModifiers::CONTROL,
                 }) => {
-                    let _ = execute!(stdout(), LeaveAlternateScreen);
-                    let _ = disable_raw_mode();
-                    let _ = execute!(stdout(), Show);
+                    execute!(stdout(), LeaveAlternateScreen)?;
+                    disable_raw_mode()?;
+                    execute!(stdout(), Show)?;
                     std::process::exit(0);
                 }
                 // If Down arrow is pressed, add 1 to the marker and update the string
@@ -69,8 +70,7 @@ fn init(mutex: &Lines) {
                     modifiers: KeyModifiers::NONE,
                 }) => {
                     upper_mark += 1;
-                    // FIXME(poliorcetics): handle unused Result.
-                    let _ = draw(&mutex.lock().unwrap(), rows, &mut upper_mark);
+                    draw(&mutex.lock().unwrap(), rows, &mut upper_mark)?;
                 }
                 // If Up arrow is pressed, subtract 1 from the marker and update the string
                 Event::Key(KeyEvent {
@@ -78,14 +78,12 @@ fn init(mutex: &Lines) {
                     modifiers: KeyModifiers::NONE,
                 }) => {
                     upper_mark = upper_mark.saturating_sub(1);
-                    // FIXME(poliorcetics): handle unused Result.
-                    let _ = draw(&mutex.lock().unwrap(), rows, &mut upper_mark);
+                    draw(&mutex.lock().unwrap(), rows, &mut upper_mark)?;
                 }
                 // When terminal is resized, update the rows and redraw
                 Event::Resize(_, height) => {
                     rows = height as usize;
-                    // FIXME(poliorcetics): handle unused Result.
-                    let _ = draw(&mutex.lock().unwrap(), rows, &mut upper_mark);
+                    draw(&mutex.lock().unwrap(), rows, &mut upper_mark)?;
                 }
                 _ => {}
             }
@@ -102,6 +100,11 @@ fn init(mutex: &Lines) {
 /// This function switches to the [`Alternate Screen`] of the TTY and switches
 /// to [`raw mode`].
 ///
+/// ## Errors
+///
+/// Several operations can fail when outputting information to a terminal, see
+/// the [`Result`] type.
+///
 /// ## Example
 ///
 /// ```
@@ -113,20 +116,25 @@ fn init(mutex: &Lines) {
 /// use std::time::Duration;
 ///
 /// #[tokio::main]
-/// async fn main() {
+/// async fn main() -> Result<(), Box<dyn std::error::Error>> {
 ///     let output = Arc::new(Mutex::new(String::new()));
-///     let push_data = async {
-///         for i in 1..=100 {
-///             let mut guard = output.lock().unwrap();
-///             // Always use writeln to add a \n after the line
-///             writeln!(guard, "{}", i);
-///             // If you have further asynchronous blocking code, drop the borrow here
-///             drop(guard);
-///             // Some asynchronous blocking code
-///             sleep(Duration::new(1,0)).await;
+///
+///     let increment = async {
+///         let mut counter: u8 = 0;
+///         while counter <= 30 {
+///             let mut output = output.lock().unwrap();
+///             writeln!(output, "{}", counter.to_string())?;
+///             counter += 1;
+///             drop(output);
+///             sleep(Duration::from_millis(100)).await;
 ///         }
-///    };
-///    join!(minus::tokio_updating(output.clone()), push_data);
+///         Result::<_, std::fmt::Error>::Ok(())
+///     };
+///
+///     let (res1, res2) = join!(minus::tokio_updating(output.clone()), increment);
+///     res1?;
+///     res2?;
+///     Ok(())
 /// }
 /// ```
 ///
@@ -137,11 +145,9 @@ fn init(mutex: &Lines) {
 /// [`Alternate Screen`]: crossterm::terminal#alternate-screen
 /// [`raw mode`]: crossterm::terminal#raw-mode
 #[cfg(feature = "tokio_lib")]
-pub async fn tokio_updating(mutex: Lines) {
+pub async fn tokio_updating(mutex: Lines) -> Result {
     use tokio::task;
-    task::spawn(async move {
-        init(&mutex);
-    });
+    task::spawn(async move { init(&mutex) }).await?
 }
 
 /// Initialize a updating pager inside an [`async_std task`].
@@ -153,28 +159,41 @@ pub async fn tokio_updating(mutex: Lines) {
 /// This function switches to the [`Alternate Screen`] of the TTY and switches
 /// to [`raw mode`].
 ///
+/// ## Errors
+///
+/// Several operations can fail when outputting information to a terminal, see
+/// the [`Result`] type.
+///
 /// ## Example
 ///
 /// ```
+/// use async_std::task::sleep;
 /// use futures::join;
 ///
+/// use std::fmt::Write;
 /// use std::sync::{Arc, Mutex};
 /// use std::time::Duration;
 ///
 /// #[async_std::main]
-/// async fn main() {
+/// async fn main() -> Result<(), Box<dyn std::error::Error>> {
 ///     let output = Arc::new(Mutex::new(String::new()));
-///     let push_data = async {
-///         for i in 1..=100 {
-///             let mut guard = output.lock().unwrap();
-///             guard.push_str(&i.to_string());
-///             // If you have further asynchronous blocking code, drop the borrow here
-///             drop(guard);
-///             // Some asynchronous blocking code
-///             async_std::task::sleep(Duration::new(1,0)).await;
+///
+///     let increment = async {
+///         let mut counter: u8 = 0;
+///         while counter <= 30 {
+///             let mut output = output.lock().unwrap();
+///             writeln!(output, "{}", counter.to_string())?;
+///             counter += 1;
+///             drop(output);
+///             sleep(Duration::from_millis(100)).await;
 ///         }
-///    };
-///    join!(minus::async_std_updating(output.clone()), push_data);
+///         Result::<_, std::fmt::Error>::Ok(())
+///     };
+///
+///     let (res1, res2) = join!(minus::async_std_updating(output.clone()), increment);
+///     res1?;
+///     res2?;
+///     Ok(())
 /// }
 /// ```
 ///
@@ -186,10 +205,7 @@ pub async fn tokio_updating(mutex: Lines) {
 /// [`Alternate Screen`]: crossterm::terminal#alternate-screen
 /// [`raw mode`]: crossterm::terminal#raw-mode
 #[cfg(feature = "async_std_lib")]
-pub async fn async_std_updating(mutex: Lines) {
+pub async fn async_std_updating(mutex: Lines) -> Result {
     use async_std::task;
-    task::spawn(async move {
-        init(&mutex);
-    })
-    .await;
+    task::spawn(async move { init(&mutex) }).await
 }
--- src/static_pager.rs
+++ src/static_pager.rs
@@ -28,14 +28,14 @@ use std::io::{stdout, Write};
 ///
 /// ```
 /// use std::fmt::Write;
-/// 
+///
 /// fn main() -> minus::Result<(), Box<dyn std::error::Error>> {
 ///     let mut output = String::new();
-/// 
+///
 ///     for i in 1..=30 {
 ///         writeln!(output, "{}", i)?;
 ///     }
-/// 
+///
 ///     minus::page_all(&output)?;
 ///     Ok(())
 /// }
@@ -113,5 +113,3 @@ pub fn page_all(lines: &str) -> Result {
         }
     }
 }
-
-
