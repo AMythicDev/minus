b883de0cb661428ce0859896791467d24b5f9e67
Alexis Bourget <alexis.bourget@gmail.com>
2020-12-07 19:18:54 +0100

    Update the static example to use the minus::Result type

--- examples/static.rs
+++ examples/static.rs
@@ -1,9 +1,9 @@
 use std::fmt::Write;
 
-fn main() {
+fn main() -> minus::Result {
     let mut output = String::new();
     for i in 1..=30 {
         let _ = writeln!(output, "{}", i);
     }
-    minus::page_all(&output);
+    minus::page_all(&output)
 }
