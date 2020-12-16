74a31ccb3282fc2c3683840f6cae519d6c880b89
Arijit Dey <arijid79@gmail.com>
2020-12-16 09:46:42 +0530

    Fix doc test

--- src/static_pager.rs
+++ src/static_pager.rs
@@ -48,7 +48,7 @@ pub enum PageAllError {
 /// use std::fmt::Write;
 ///
 /// fn main() -> Result<(), Box<dyn std::error::Error>> {
-///     let mut output = minus::Pager::new().finish();
+///     let mut output = minus::Pager::new();
 ///
 ///     for i in 0..=30 {
 ///         writeln!(output.lines, "{}", i)?;
