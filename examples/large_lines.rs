2784b71aca47fd4cd8dd3db7dc30d864e721fa81
Arijit Dey <arijid79@gmail.com>
2021-02-17 23:51:14 +0530

    Add new examples for working in new APIs

--- examples/large_lines.rs
+++ examples/large_lines.rs
@@ -3,8 +3,10 @@ use std::fmt::Write;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut output = minus::Pager::new();
 
-    for i in 0..=30 {
-        writeln!(output.lines, "{}", i)?;
+    for _ in 0..30 {
+        for _ in 0..=30 {
+            output.push_str("Hello ")
+        }
     }
 
     minus::page_all(output)?;
