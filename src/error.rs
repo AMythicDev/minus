bc7c181dbd029647b3f816d173db763f0c56aa95
rezural <rezural@protonmail.com>
2021-02-21 13:18:39 +1100

    remove unused error

--- src/error.rs
+++ src/error.rs
@@ -61,8 +61,6 @@ pub enum CleanupError {
     #[error("Failed to switch back to main screen")]
     LeaveAlternateScreen(TermError),
 
-    #[error("Couldn't determine the terminal size")]
-    TerminalSize(TermError),
 }
 
 /// Errors that can happen while running
