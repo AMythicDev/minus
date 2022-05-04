e9f841cbe5b365655644d20244265467bfc061a9
Arijit Dey <arijit79@protonmail.com>
2022-05-05 00:28:49 +0530

    feat: Introduce native input matching definition
    This set of changes will allow minus to define key, mouse and resize
    events to be declared in natively and not requiring the end application
    to interact with the underlying crossterm crate.
    
    This means that keybindings like Ctrl+L can be declared into minus with
    appropriate function programatically using something like this
    
                pager.add_key_event("c-l", |event, pagerstate| {
                    ...
                }));
    
    This gives advantage to the end-applications as now they get the ability
    to add to, modify, or remove minus's default event binding without needing
    to rewrite the entire matcher function by themselves.

---
+++ src/input/definitions/mousedefs.rs
@@ -0,0 +1,190 @@
+use std::collections::HashMap;
+
+use super::{Token, MODIFIERS};
+use crossterm::event::{KeyModifiers, MouseButton, MouseEvent, MouseEventKind};
+use once_cell::sync::Lazy;
+
+static MOUSE_ACTIONS: Lazy<HashMap<&str, MouseEventKind>> = Lazy::new(|| {
+    let mut map = HashMap::new();
+
+    map.insert("left:down", MouseEventKind::Down(MouseButton::Left));
+    map.insert("right:down", MouseEventKind::Down(MouseButton::Right));
+    map.insert("mid:down", MouseEventKind::Down(MouseButton::Middle));
+
+    map.insert("left:up", MouseEventKind::Up(MouseButton::Left));
+    map.insert("right:up", MouseEventKind::Up(MouseButton::Right));
+    map.insert("mid:up", MouseEventKind::Up(MouseButton::Middle));
+
+    map.insert("left:drag", MouseEventKind::Drag(MouseButton::Left));
+    map.insert("right:drag", MouseEventKind::Drag(MouseButton::Right));
+    map.insert("mid:drag", MouseEventKind::Drag(MouseButton::Middle));
+
+    map.insert("move", MouseEventKind::Moved);
+    map.insert("scrollup", MouseEventKind::ScrollUp);
+    map.insert("scrolldown", MouseEventKind::ScrollDown);
+
+    map
+});
+
+pub fn parse_mouse_event(text: &str) -> MouseEvent {
+    let token_list = super::parse_tokens(text);
+    gen_mouse_event_from_tokenlist(&token_list, text)
+}
+
+fn gen_mouse_event_from_tokenlist(token_list: &[Token], text: &str) -> MouseEvent {
+    let mut kind = None;
+    let mut modifiers = KeyModifiers::NONE;
+
+    let mut token_iter = token_list.iter().peekable();
+
+    while let Some(token) = token_iter.peek() {
+        match token {
+            Token::Separator => {
+                token_iter.next();
+                assert!(
+                    !(token_iter.peek() == Some(&&Token::Separator)),
+                    "'{}': Multiple separators found consecutively",
+                    text
+                );
+            }
+            Token::SingleChar(c) => {
+                token_iter.next();
+                MODIFIERS.get(c).map_or_else(
+                    || {
+                        panic!("'{}': Invalid keymodifier '{}' given", text, c);
+                    },
+                    |m| {
+                        if token_iter.next() == Some(&Token::Separator) {
+                            assert!(
+                                !modifiers.contains(*m),
+                                "'{}': Multiple instances of same modifier given",
+                                text
+                            );
+                            modifiers.insert(*m);
+                        } else {
+                            panic!("'{}' Invalid key input sequence given", text);
+                        }
+                    },
+                );
+            }
+            Token::MultipleChar(c) => {
+                let c = c.to_ascii_lowercase().to_string();
+                MOUSE_ACTIONS.get(c.as_str()).map_or_else(
+                    || panic!("'{}': Invalid key input sequence given", text),
+                    |k| {
+                        if kind.is_none() {
+                            kind = Some(*k);
+                        } else {
+                            panic!("'{}': Invalid key input sequence given", text);
+                        }
+                    },
+                );
+                token_iter.next();
+            }
+        }
+    }
+    MouseEvent {
+        kind: kind.unwrap_or_else(|| panic!("No MouseEventKind found for '{}", text)),
+        modifiers,
+        row: 0,
+        column: 0,
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::parse_mouse_event;
+    use crossterm::event::{KeyModifiers, MouseButton, MouseEvent, MouseEventKind};
+
+    #[test]
+    fn test_without_modifiers() {
+        assert_eq!(
+            parse_mouse_event("left:down"),
+            MouseEvent {
+                kind: MouseEventKind::Down(MouseButton::Left),
+                modifiers: KeyModifiers::NONE,
+                row: 0,
+                column: 0,
+            }
+        );
+
+        assert_eq!(
+            parse_mouse_event("mid:up"),
+            MouseEvent {
+                kind: MouseEventKind::Up(MouseButton::Middle),
+                modifiers: KeyModifiers::NONE,
+                row: 0,
+                column: 0,
+            }
+        );
+
+        assert_eq!(
+            parse_mouse_event("right:down"),
+            MouseEvent {
+                kind: MouseEventKind::Down(MouseButton::Right),
+                modifiers: KeyModifiers::NONE,
+                row: 0,
+                column: 0,
+            }
+        );
+        assert_eq!(
+            parse_mouse_event("scrollup"),
+            MouseEvent {
+                kind: MouseEventKind::ScrollUp,
+                modifiers: KeyModifiers::NONE,
+                row: 0,
+                column: 0,
+            }
+        );
+        assert_eq!(
+            parse_mouse_event("move"),
+            MouseEvent {
+                kind: MouseEventKind::Moved,
+                modifiers: KeyModifiers::NONE,
+                row: 0,
+                column: 0,
+            }
+        );
+    }
+
+    #[test]
+    fn test_with_modifiers() {
+        assert_eq!(
+            parse_mouse_event("m-left:down"),
+            MouseEvent {
+                kind: MouseEventKind::Down(MouseButton::Left),
+                modifiers: KeyModifiers::ALT,
+                row: 0,
+                column: 0,
+            }
+        );
+
+        assert_eq!(
+            parse_mouse_event("m-c-mid:up"),
+            MouseEvent {
+                kind: MouseEventKind::Up(MouseButton::Middle),
+                modifiers: KeyModifiers::ALT | KeyModifiers::CONTROL,
+                row: 0,
+                column: 0,
+            }
+        );
+        assert_eq!(
+            parse_mouse_event("c-scrollup"),
+            MouseEvent {
+                kind: MouseEventKind::ScrollUp,
+                modifiers: KeyModifiers::CONTROL,
+                row: 0,
+                column: 0,
+            }
+        );
+        assert_eq!(
+            parse_mouse_event("m-c-s-move"),
+            MouseEvent {
+                kind: MouseEventKind::Moved,
+                modifiers: KeyModifiers::all(),
+                row: 0,
+                column: 0,
+            }
+        );
+    }
+}
---
+++ src/input/event_wrapper.rs
@@ -0,0 +1,176 @@
+use super::{InputClassifier, InputEvent};
+use crate::PagerState;
+use crossterm::event::{Event, MouseEvent};
+use std::{
+    collections::hash_map::RandomState, collections::HashMap, hash::BuildHasher, hash::Hash,
+    sync::Arc,
+};
+
+type EventReturnType = Arc<dyn Fn(Event, &PagerState) -> InputEvent + Send + Sync>;
+
+pub struct HashedEventRegister<S>(HashMap<EventWrapper, EventReturnType, S>);
+
+#[derive(Copy, Clone, Eq)]
+enum EventWrapper {
+    ExactMatchEvent(Event),
+    WildEvent,
+}
+
+impl From<Event> for EventWrapper {
+    fn from(e: Event) -> Self {
+        Self::ExactMatchEvent(e)
+    }
+}
+
+impl From<&Event> for EventWrapper {
+    fn from(e: &Event) -> Self {
+        Self::ExactMatchEvent(*e)
+    }
+}
+
+impl PartialEq for EventWrapper {
+    fn eq(&self, other: &Self) -> bool {
+        match (self, other) {
+            (
+                Self::ExactMatchEvent(Event::Mouse(MouseEvent {
+                    kind, modifiers, ..
+                })),
+                Self::ExactMatchEvent(Event::Mouse(MouseEvent {
+                    kind: o_kind,
+                    modifiers: o_modifiers,
+                    ..
+                })),
+            ) => kind == o_kind && modifiers == o_modifiers,
+            (
+                Self::ExactMatchEvent(Event::Resize(..)),
+                Self::ExactMatchEvent(Event::Resize(..)),
+            )
+            | (Self::WildEvent, Self::WildEvent) => true,
+            (Self::ExactMatchEvent(ev), Self::ExactMatchEvent(o_ev)) => ev == o_ev,
+            _ => false,
+        }
+    }
+}
+
+impl Hash for EventWrapper {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        let tag = std::mem::discriminant(self);
+        tag.hash(state);
+        match self {
+            Self::ExactMatchEvent(Event::Mouse(MouseEvent {
+                kind, modifiers, ..
+            })) => {
+                kind.hash(state);
+                modifiers.hash(state);
+            }
+            Self::WildEvent | Self::ExactMatchEvent(Event::Resize(..)) => {}
+            Self::ExactMatchEvent(v) => {
+                v.hash(state);
+            }
+        }
+    }
+}
+
+impl<S> HashedEventRegister<S>
+where
+    S: BuildHasher,
+{
+    fn new(s: S) -> Self {
+        Self(HashMap::with_hasher(s))
+    }
+
+    pub fn insert_wild_event_matcher(
+        &mut self,
+        v: impl Fn(Event, &PagerState) -> InputEvent + Send + Sync + 'static,
+    ) {
+        self.0.insert(EventWrapper::WildEvent, Arc::new(v));
+    }
+
+    pub fn get(&self, k: &Event) -> Option<&EventReturnType> {
+        self.0
+            .get(&k.into())
+            .map_or_else(|| self.0.get(&EventWrapper::WildEvent), |k| Some(k))
+    }
+
+    pub fn add_resize_event(
+        &mut self,
+        v: impl Fn(Event, &PagerState) -> InputEvent + Send + Sync + 'static,
+    ) {
+        let v = Arc::new(v);
+        self.0
+            .insert(EventWrapper::ExactMatchEvent(Event::Resize(0, 0)), v);
+    }
+
+    pub fn remove_resize_event(&mut self) {
+        self.0.remove(&EventWrapper::ExactMatchEvent(Event::Resize(0, 0)));
+    }
+
+}
+
+// Key event Insertions functions
+impl<S> HashedEventRegister<S>
+where
+    S: BuildHasher,
+{
+    pub fn add_key_events(
+        &mut self,
+        keys: &[&str],
+        v: impl Fn(Event, &PagerState) -> InputEvent + Send + Sync + 'static,
+    ) {
+        let v = Arc::new(v);
+        for k in keys {
+            self.0.insert(
+                Event::Key(super::definitions::keydefs::parse_key_event(k)).into(),
+                v.clone(),
+            );
+        }
+    }
+
+    pub fn remove_key_events(&mut self, keys: &[&str]) {
+        for k in keys {
+            self.0.remove(&Event::Key(super::definitions::keydefs::parse_key_event(k)).into());
+        }
+    }
+
+}
+
+// Mouse event insertions functions
+impl<S> HashedEventRegister<S>
+where
+    S: BuildHasher,
+{
+    pub fn add_mouse_events(
+        &mut self,
+        keys: &[&str],
+        v: impl Fn(Event, &PagerState) -> InputEvent + Send + Sync + 'static,
+    ) {
+        let v = Arc::new(v);
+        for k in keys {
+            self.0.insert(
+                Event::Mouse(super::definitions::mousedefs::parse_mouse_event(k)).into(),
+                v.clone(),
+            );
+        }
+    }
+
+    pub fn remove_mouse_events(&mut self, keys: &[&str]) {
+        for k in keys {
+            self.0.remove(&Event::Mouse(super::definitions::mousedefs::parse_mouse_event(k)).into());
+        }
+    }
+}
+
+impl Default for HashedEventRegister<RandomState> {
+    fn default() -> Self {
+        Self::new(RandomState::new())
+    }
+}
+
+impl<S> InputClassifier for HashedEventRegister<S>
+where
+    S: BuildHasher,
+{
+    fn classify_input(&self, ev: Event, ps: &crate::PagerState) -> Option<InputEvent> {
+        self.get(&ev).map(|c| c(ev, ps))
+    }
+}
