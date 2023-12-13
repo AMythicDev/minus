602957f23d06d67a621e1c5d4e9bac81e3f8fcad
TornaxO7 <tornax@proton.me>
2023-12-13 20:20:06 +0100

    chore(nix): Adding rust-toolchain and improve flake (#112)
    * adding rust-toolchain and improving flake
    
    * flake: removing description
    
    * update flake.nix and add msrv devShell

--- flake.nix
+++ flake.nix
@@ -1,29 +1,39 @@
- {
- description = "Home Manager configuration of arijit";
-
+{
   inputs = {
     # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.url =  "github:numtide/flake-utils";
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, rust-overlay, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem(system:
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
       let
         overlays = [ (import rust-overlay) ];
         pkgs = import nixpkgs {
           inherit system overlays;
         };
-        in
-        with pkgs;
-        {
-          devShells.default = mkShell {
-            buildInputs = [
-              rust-bin.stable."1.67.0".default
-              just
-            ];
+
+        rust-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+        rust-nightly-toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain-nightly.toml;
+
+        shared_packages = with pkgs; [
+          just
+        ];
+      in
+      with pkgs;
+      {
+        devShells = rec {
+          default = msrv;
+
+          nightly = mkShell {
+            packages = shared_packages ++ [ rust-nightly-toolchain ];
           };
-        }
-      );
-  }
+
+          msrv = mkShell {
+            packages = shared_packages ++ [ rust-toolchain ];
+          };
+        };
+      }
+    );
+}
