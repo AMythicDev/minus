76b45a4d121bccebcdb6e7259c24237c7597cdd6
TornaxO7 <tornax@proton.me>
2023-12-21 17:25:49 +0100

    removing rust toolchain and set toolchain only in flake

--- flake.nix
+++ flake.nix
@@ -14,7 +14,7 @@
           inherit system overlays;
         };
 
-        rust-toolchain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+        rust-toolchain = pkgs.rust-bin.stable."1.67.0".default.override {
           extensions = [ "rust-src" "rust-analyzer" ];
         };
 
